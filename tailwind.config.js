beb5b0fe9308ba165a2f910d9867857aec4cfa43
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-05 14:08:57 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        'ap-blue': '#1e3a8a',
+        'ap-amber': '#fbbf24',
+        'ap-slate': '#1f2937',
+        'ap-cyan': '#22d3ee',
+      },
+    },
+  },
   plugins: [],
 };
