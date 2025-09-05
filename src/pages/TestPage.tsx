0d44fa6f6ed8a43d2430aba619ab2cc2c9046e77
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-05 16:20:47 -0400

    Create TestPage.tsx

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen text-white bg-blue-950">
+            <h1 className="text-4xl">This is the Test Page</h1>
+        </div>
+    );
+};
+
+export default TestPage;
