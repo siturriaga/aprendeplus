88c17aa579b7b630d796f8177db25c3dcdc73dfa
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-08 10:20:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'blue-950': '#040558',
-        'amber-100': '#fff8e1',
-        'amber-200': '#ffe0b2',
-        'amber-300': '#ffb300',
-        'amber-400': '#ff9800',
-        'amber-500': '#ff6f00',
-        'blue-700': '#1976d2',
-        'blue-800': '#1565c0',
-        'blue-900': '#0d47a1',
+        'ap-blue': '#1e3a8a',
+        'ap-amber': '#fbbf24',
+        'ap-slate': '#1f2937',
+        'ap-cyan': '#22d3ee',
       },
     },
   },
   plugins: [],
-}
+};
