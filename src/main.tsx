1419580dcbfbc93228eaa1b3c4090a9498f60d1f
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-11 11:43:17 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./globals.css";
 import "./styles.css";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element #root not found");
-
-createRoot(root).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
