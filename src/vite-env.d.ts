85b6e2ea7afa86380f4541a6eec93ab9d1f352f3
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-12 11:39:14 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_EMAILJS_SERVICE_ID: string;
   readonly VITE_EMAILJS_TEMPLATE_ID: string;
   readonly VITE_EMAILJS_PUBLIC_KEY: string;
-  // agrega aquí cualquier otra clave VITE_* que uses
 }
 
 interface ImportMeta {
