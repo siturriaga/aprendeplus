f075a50d95be599f770769567ad82a97318d3b67
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-16 13:06:45 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // This line is crucial!
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
