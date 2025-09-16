36bfee1f2f68f48dc98cfbb92a12bb06a57f4403
siturriaga <iturriaga.sebastian.e@gmail.com>
2025-09-16 15:25:16 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-// This path now correctly points to your file structure
-import { AccessibilityProvider } from './components/AccessibilityContext.tsx'; 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AccessibilityProvider>
-      <App />
-    </AccessibilityProvider>
-  </React.StrictMode>,
-);
+    <App />
+  </React.StrictMode>
+)
